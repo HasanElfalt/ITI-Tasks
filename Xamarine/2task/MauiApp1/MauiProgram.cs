e4daf56d03cb3e49a10087196ed0bff7ad6e0b14
Hasan Abdelbast Abdelrahman <hassanelfalt60@gmail.com>
2023-05-18 09:07:46 +0300

    Add files via upload

---
+++ Xamarine/2task/MauiApp1/MauiProgram.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Maui;
+
+namespace MauiApp1;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+		builder.UseMauiApp<App>().UseMauiCommunityToolkit();
+		return builder.Build();
+	}
+}
